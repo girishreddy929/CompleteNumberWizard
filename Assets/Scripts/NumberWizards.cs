8dfd68bce8860cd3ca65b41b7cb49189001b9119
giri929 <reddygirish929@gmail.com>
2018-02-11 22:55:51 +0530

    Complete files

---
+++ Assets/Scripts/NumberWizards.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizards : MonoBehaviour {
+
+	int max = 1001;
+	int min = 1;
+	int guess=500;
+
+	void Start () {
+		StartGame ();
+		}
+	void StartGame (){
+		max = 1001;
+		min = 1;
+		guess = 500;
+		print ("===================");
+
+		print ("Welcome to Number Wizard ");
+		print ("Select a number and don't tell me!");
+
+		print ("Select a Number in less than :     " + (max-1));
+		print ("Select a number in more than :     " + min);
+
+
+		print ("Is your number more than or less than "+ guess);
+		print ("up arrow = higher,down arrow = lower and return = equal");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			min = guess;
+			NextGuess ();
+			}
+		else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			max = guess;
+			NextGuess ();
+		}
+		else if (Input.GetKeyDown (KeyCode.Return)) {
+			print ("I won!");
+			StartGame ();
+		}
+
+	}
+
+	void NextGuess(){
+		guess = (min + max) / 2;
+		print ("Higher or lower than " + guess);
+		print ("up arrow = higher,down arrow = lower and return = equal");
+	}
+}
